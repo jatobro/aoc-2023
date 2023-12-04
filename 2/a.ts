4985382b204355136f95d77fd92565f0af7c1b84
jatobro <jakob.tossebro@gmail.com>
2023-12-04 22:33:23 +0100

    day 2 part one

---
+++ 2/a.ts
@@ -0,0 +1,56 @@
+const input = await Deno.readTextFile("./input.txt");
+
+const lines = input.split("\n");
+
+console.log("lines", lines);
+
+const RED_MAX = 12;
+const GREEN_MAX = 13;
+const BLUE_MAX = 14;
+
+const games = lines.map((line) =>
+  line
+    .split(":")[1]
+    .split(";")
+    .map((set) =>
+      set
+        .trim()
+        .split(",")
+        .map((reveal) => reveal.trim().split(" "))
+    )
+);
+
+console.log("games", games);
+
+let sum = 0;
+
+games.forEach((sets) => {
+  let isPossible = true;
+
+  sets.forEach((set) => {
+    let r = 0,
+      g = 0,
+      b = 0;
+
+    set.forEach((reveal) => {
+      const amount = Number(reveal[0]);
+      const color = reveal[1];
+
+      if (color === "red") {
+        r += amount;
+      } else if (color === "green") {
+        g += amount;
+      } else {
+        b += amount;
+      }
+    });
+
+    if (r > RED_MAX || g > GREEN_MAX || b > BLUE_MAX) {
+      isPossible = false;
+    }
+  });
+
+  sum += isPossible ? games.indexOf(sets) + 1 : 0;
+});
+
+console.log("sum", sum);
