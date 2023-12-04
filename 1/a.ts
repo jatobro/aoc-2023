7e2ce3fdfdfa57468b42b1723633fb3637158c44
jatobro <jakob.tossebro@gmail.com>
2023-12-04 01:43:09 +0100

    do day 1 first part

--- 1/a.ts
+++ 1/a.ts
@@ -1,2 +1,19 @@
 const input = await Deno.readTextFile("./input.txt");
-console.log(input);
+
+const lines = input.split("\n");
+
+console.log("input", lines);
+
+const values = lines.map((line) => {
+  const dLine = line.replace(/\D/g, "");
+
+  return Number(dLine[0] + dLine[dLine.length - 1]);
+});
+
+console.log("values", values);
+
+let sum = 0;
+
+values.forEach((value) => (sum += value));
+
+console.log("sum", sum);
