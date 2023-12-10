d31e2e0ea041d5b7616919a6f8d659033bf2e653
jatobro <jakob.tossebro@gmail.com>
2023-12-10 14:42:04 +0100

    day 7 first part

--- 1/b.ts
+++ 1/b.ts
@@ -5,7 +5,9 @@ const lines = input.split("\n");
 console.log("lines", lines);
 
 const values = lines.map((line) => {
-  const replacements = [
+  let newLine = line;
+
+  let replacements = [
     { from: "one", to: "1" },
     { from: "two", to: "2" },
     { from: "three", to: "3" },
@@ -17,23 +19,44 @@ const values = lines.map((line) => {
     { from: "nine", to: "9" },
   ];
 
+  replacements = replacements.filter((replacement) =>
+    line.includes(replacement.from)
+  );
+
   replacements.sort((a, b) => line.indexOf(a.from) - line.indexOf(b.from));
 
-  let noWordDigits = line;
+  if (lines.indexOf(line) < 5) {
+    console.log(replacements);
+  }
 
-  replacements.forEach((replacement) => {
-    noWordDigits = noWordDigits.replace(
-      new RegExp(replacement.from, "g"),
-      replacement.to
+  if (replacements.length !== 0) {
+    newLine = newLine.replace(
+      new RegExp(replacements[0].from),
+      replacements[0].to
     );
-  });
+  }
 
-  const digits = noWordDigits.replace(/\D/g, "");
+  replacements.sort((a, b) => line.indexOf(b.from) - line.indexOf(a.from));
+
+  if (lines.indexOf(line) < 5) {
+    console.log(replacements);
+  }
+
+  if (replacements.length !== 0) {
+    newLine = newLine.replace(
+      new RegExp(replacements[0].from),
+      replacements[0].to
+    );
+  }
+
+  const digits = newLine.replace(/\D/g, "");
 
   return Number(digits[0] + digits[digits.length - 1]);
 });
 
-console.log("values", values);
+for (let i = 0; i < lines.length; i++) {
+  console.log("LINE:", lines[i], "VALUE:", values[i]);
+}
 
 let sum = 0;
 
