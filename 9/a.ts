9b7abebc553b49c42bf8feb5cfb654a4c8f2581c
jatobro <jakob.tossebro@gmail.com>
2023-12-22 16:33:11 +0100

    day 9 first part

--- 9/a.ts
+++ 9/a.ts
@@ -1,5 +1,50 @@
-const input = await Deno.readTextFile("./input.txt");
+type Sequence = number[];
 
-const lines = input.split("\n");
+const lines = (await Deno.readTextFile("./input.txt")).split("\n");
 
 console.log("lines", lines);
+
+const sequences: Sequence[] = lines.map((line) => line.split(" ").map(Number));
+
+console.log("sequences", sequences);
+
+const predictions = sequences.map((sequence) => {
+  const diffSequences = [sequence];
+
+  while (
+    !diffSequences[diffSequences.length - 1].every((number) => number === 0)
+  ) {
+    const previousSequence = diffSequences[diffSequences.length - 1];
+
+    diffSequences.push([]);
+    previousSequence.forEach((number, idx) => {
+      if (idx !== previousSequence.length - 1) {
+        diffSequences[diffSequences.length - 1].push(
+          previousSequence[idx + 1] - number
+        );
+      }
+    });
+  }
+
+  const reversedSequence = diffSequences.reverse();
+  reversedSequence[0].push(0);
+
+  for (let i = 1; i < reversedSequence.length; i++) {
+    reversedSequence[i].push(
+      reversedSequence[i - 1][reversedSequence[i - 1].length - 1] +
+        reversedSequence[i][reversedSequence[i].length - 1]
+    );
+  }
+
+  const predictionSequence = reversedSequence.reverse()[0];
+
+  const prediction = predictionSequence[predictionSequence.length - 1];
+
+  return prediction;
+});
+
+console.log("predictions", predictions);
+
+const solution = predictions.reduce((acc, prediction) => acc + prediction);
+
+console.log("solution", solution);
